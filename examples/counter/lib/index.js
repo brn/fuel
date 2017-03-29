fe9e40b8215dc936e1da57b7d7fd5cd0ec10ff08
brn <dobaw20@gmail.com>
2017-03-30 02:43:07 +0900

    update examples

--- examples/counter/lib/index.js
+++ examples/counter/lib/index.js
@@ -6,83 +6,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
 var fueldom_1 = require("fueldom");
-var PATH_REGEXP = /^\:(.+)$/;
-function parseURL(url) {
-    var path = url || location.hash.slice(1);
-    if (!path) {
-        path = '/';
-    }
-    if (path.charAt(0) !== '/') {
-        path = "/" + path;
-    }
-    if (path.length > 1 && path.charAt(path.length - 1) === '/') {
-        path = path.slice(0, path.length - 1);
-    }
-    return path;
-}
-var EmptyRoot = function (_a) {
-    var children = _a.children;
-    return ({ children: children });
-};
-var Router = (function (_super) {
-    tslib_1.__extends(Router, _super);
-    function Router(p, c) {
-        var _this = _super.call(this, p, c) || this;
-        _this.state = { url: parseURL() };
-        window.addEventListener('hashchange', function (event) {
-            var url = event.newURL.match(/#(.+)/);
-            _this.setState({ url: parseURL(url ? url[1] : '/') });
-        }, false);
-        return _this;
-    }
-    Router.prototype.render = function () {
-        var _this = this;
-        return fueldom_1.React.createElement("div", null, fueldom_1.Fuel.Children.map(this.props.children, function (child) { return fueldom_1.Fuel.cloneElement(child, { location: _this.state.url }); }));
-    };
-    return Router;
-}(fueldom_1.Fuel.Component));
-var Route = (function (_super) {
-    tslib_1.__extends(Route, _super);
-    function Route() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.params = {};
-        return _this;
-    }
-    Route.prototype.render = function () {
-        var _this = this;
-        var Component = this.props.component;
-        var location = this.props.location;
-        var parent = this.parent;
-        if (this.matchs && this.matchs.length > 0) {
-            var children = fueldom_1.Fuel.Children.map(this.props.children, function (child) { return fueldom_1.Fuel.cloneElement(child, { location: location, parent: parent, params: _this.params }); });
-            if (Component) {
-                return fueldom_1.React.createElement(Component, { params: this.params }, children);
-            }
-            return fueldom_1.React.createElement("div", null, children);
-        }
-        else if (fueldom_1.Fuel.Children.toArray(this.props.children).filter(function (child) { return child.type === Route; }).length) {
-            return fueldom_1.React.createElement("div", null, fueldom_1.Fuel.Children.map(this.props.children, function (child) { return fueldom_1.Fuel.cloneElement(child, { location: location, parent: parent }); }));
-        }
-        return null;
-    };
-    Route.prototype.componentWillMount = function () {
-        var _this = this;
-        var location = this.props.location;
-        this.parent = "" + (this.props.parent ? this.props.parent : '') + parseURL(this.props.path);
-        var match;
-        var ids = [];
-        var replaced = this.parent.replace(/\:[^\/]+/g, function (id) {
-            ids.push(id.slice(1));
-            return '([^/]+)';
-        });
-        var regexp = new RegExp("^" + replaced + "$");
-        this.matchs = location.match(regexp);
-        if (this.matchs && this.matchs.length > 0) {
-            this.matchs.slice(1).forEach(function (match, index) { return _this.params[ids[index]] = match; });
-        }
-    };
-    return Route;
-}(fueldom_1.Fuel.Component));
+var fuel_router_1 = require("fuel-router");
 var Counter = (function (_super) {
     tslib_1.__extends(Counter, _super);
     function Counter() {
@@ -130,9 +54,9 @@ var HandleId = (function (_super) {
     };
     return HandleId;
 }(fueldom_1.Fuel.Component));
-fueldom_1.FuelDOM.render((fueldom_1.React.createElement(Router, null,
-    fueldom_1.React.createElement(Route, { path: "/", component: Counter }),
-    fueldom_1.React.createElement(Route, { path: "/test" },
-        fueldom_1.React.createElement(Route, { path: "foo", component: Test }),
-        fueldom_1.React.createElement(Route, { path: "bar" },
-            fueldom_1.React.createElement(Route, { path: ":id", component: HandleId }))))), document.getElementById('app'));
+fueldom_1.FuelDOM.render((fueldom_1.React.createElement(fuel_router_1.Router, null,
+    fueldom_1.React.createElement(fuel_router_1.Route, { path: "/", component: Counter }),
+    fueldom_1.React.createElement(fuel_router_1.Route, { path: "/test" },
+        fueldom_1.React.createElement(fuel_router_1.Route, { path: "foo", component: Test }),
+        fueldom_1.React.createElement(fuel_router_1.Route, { path: "bar" },
+            fueldom_1.React.createElement(fuel_router_1.Route, { path: ":id", component: HandleId }))))), document.getElementById('app'));
--- examples/counter/src/index.tsx
+++ examples/counter/src/index.tsx
@@ -9,90 +9,10 @@ import {
   Fuel,
   React
 } from 'fueldom';
-
-
-const PATH_REGEXP = /^\:(.+)$/;
-
-
-function parseURL(url?: string) {
-  let path = url || location.hash.slice(1);
-
-  if (!path) {
-    path = '/';
-  }
-
-  if (path.charAt(0) !== '/') {
-    path = `/${path}`;
-  }
-
-  if (path.length > 1 && path.charAt(path.length - 1) === '/') {
-    path = path.slice(0, path.length - 1);
-  }
-
-  return path;
-}
-
-
-const EmptyRoot = ({children}) => ({children});
-
-
-class Router extends Fuel.Component<any, any> {
-  public state = {url: parseURL()}
-
-  constructor(p, c) {
-    super(p, c)
-    window.addEventListener('hashchange', event => {
-      const url = event.newURL.match(/#(.+)/);
-      this.setState({url: parseURL(url? url[1]: '/')});
-    }, false);
-  }
-
-  public render() {
-    return <div>{Fuel.Children.map(this.props.children, child => Fuel.cloneElement(child, {location: this.state.url}))}</div>
-  }
-}
-
-
-class Route extends Fuel.Component<any, any> {
-  private matchs;
-
-  private params = {};
-
-  private parent;
-
-  public render() {
-    const Component = this.props.component;
-    const location = this.props.location;
-    const parent = this.parent;
-    if (this.matchs && this.matchs.length > 0) {
-      const children = Fuel.Children.map(this.props.children, child => Fuel.cloneElement(child, {location, parent, params: this.params}));
-      if (Component) {
-        return <Component params={this.params}>{children}</Component>;
-      }
-      return <div>{children}</div>;
-    } else if (Fuel.Children.toArray(this.props.children).filter(child => child.type === Route).length) {
-      return <div>{Fuel.Children.map(this.props.children, child => Fuel.cloneElement(child, {location, parent}))}</div>;
-    }
-    return null;
-  }
-
-
-  public componentWillMount() {
-    const {location} = this.props;
-    this.parent = `${this.props.parent? this.props.parent: ''}${parseURL(this.props.path)}`;
-    let match
-    const ids = [];
-    const replaced = this.parent.replace(/\:[^\/]+/g, id => {
-      ids.push(id.slice(1));
-      return '([^/]+)';
-    });
-    const regexp = new RegExp(`^${replaced}$`);
-    this.matchs = location.match(regexp);
-    if (this.matchs && this.matchs.length > 0) {
-      this.matchs.slice(1).forEach((match, index) => this.params[ids[index]] = match);
-    }
-  }
-}
+import {
+  Router,
+  Route
+} from 'fuel-router';
 
 
 class Counter extends Fuel.Component<any, any> {
