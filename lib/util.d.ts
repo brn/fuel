7772eb437995c004b4a50b383d607e737ad98056
brn <dobaw20@gmail.com>
2017-03-31 16:57:09 +0900

    MAKE gh-pages

--- lib/util.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const Symbol: any;
-export declare function invariant(condition: any, message: string, warn?: boolean): void;
-export declare function merge<T extends {
-    [key: string]: any;
-}, U extends {
-    [key: string]: any;
-}>(a: T, b: U): T & U;
-export declare const requestAnimationFrame: (cb: any) => any;
-export declare const requestIdleCallback: (cb: any) => any;
